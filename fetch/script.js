a222e7a8e9572372f8eb98dba293dcc896711ac0
Marc Turner <marc.t1967@gmail.com>
2020-04-23 20:37:28 -0400

    add fetch example

---
+++ fetch/script.js
@@ -0,0 +1,37 @@
+console.log('CONNECTED');
+
+// const url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+
+// fetch(url).then(function (data) {
+//     console.log(data);
+// });
+
+// fetch(url)
+//     .then(function (res) {
+//         console.log(res);
+//         return res.json();
+//     })
+//     .then(function (data) {
+//         console.log(data.bpi.USD.rate);
+//     });
+
+// fetch error handling
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', function () {
+    const url = 'https://api.github.com/users/colt';
+    fetch(url)
+        .then(function (request) {
+            if (!request.ok) {
+                throw Error('Custom error');
+            }
+            return request;
+        })
+        .then(function (request) {
+            console.log('FINE');
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+});
